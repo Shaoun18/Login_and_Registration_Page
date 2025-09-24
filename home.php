521bd5ca8b62d127d6587e955aac1a326ce509f1
Shaoun Chandra Shill <shaounchandrashill75@gmail.com>
2025-09-24 11:25:31 +0600

    ✅Code Formated

--- home.php
+++ home.php
@@ -1,22 +1,27 @@
 <?php
-    session_start();
-    if (! isset($_SESSION['username'])) {
-        header('location:index.php');
-    }
+session_start();
+if (! isset($_SESSION['username'])) {
+    header('location:index.php');
+}
 ?>
 <html>
+
 <head>
-<tittle><h3> Home Page </h3></tittle>
-<link rel= "stylesheet" type = "text/css" href="style.css">
+    <tittle>
+        <h3> Home Page </h3>
+    </tittle>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
-<div class='container'>
-<a class = "float-right" href = "logout.php">Logout</a>
-<h1> Welcome  to our Page
-<?php
-    echo $_SESSION['name'];
-?>
-</h1>
-</div>
+    <div class='container'>
+        <a class="float-right" href="logout.php">Logout</a>
+        <h1> Welcome to our Page
+            <?php
+            echo $_SESSION['name'];
+            ?>
+        </h1>
+    </div>
 </body>
+
 </html>
